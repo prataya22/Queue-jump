9e79d241b0f277877d1a05ad349429ecb8a79092
PRATAYA GHOSH <prataya5001@gmail.com>
2026-04-03 13:42:31 +0530

    Removed inactive Firestore initialization to fix app loading crash during login initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBgtTOOqCM1171OYqQv2OfegvzgBSdPpUk",
@@ -15,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
